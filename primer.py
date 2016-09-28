c0c75185dba1454cae762f2a7ad1455bc7054c1c
kokiddp <gabriele@fablabvda.org>
2016-09-28 12:03:05 +0200

    implemented check on input file translated captions to english

--- primer.py
+++ primer.py
@@ -1,29 +1,37 @@
 import math
 import time
+import os
 
 n = 10000000
 
-print('Caricamento lista numeri primi...')
+print('Loading input file...')
 ii = time.time()
 
-ip = open('primi.txt', 'r' )
-divisori = ip.readlines()
-pd = [int(x) for x in divisori if (int(x) < (math.sqrt(n)+1))]
-pd.remove(1)
-ip.close()
+if os.path.exists("./input.txt"):
+	print('"input.txt found! This will speed things up!')
+	print('Loading input file...')
+	ip = open('input.txt', 'r' )
+	divisori = ip.readlines()
+	pd = [int(x) for x in divisori if (int(x) < (math.sqrt(n)+1))]
+	pd.remove(1)
+	ip.close()
+else:
+	print('"input.txt" not found.')
+	pd = range(2, int(math.sqrt(n))+1)
+
 
 ti = (time.time()) - ii
-print('Caricamento completato. Tempo richiesto:',ti,'secondi')
+print('Divisor generation complete. Time elapsed:',ti,'seconds')
 
 time.sleep(2)
-print('Inizio calcolo numeri primi minori di', n,'fra 3 secondi')
+print('I will begin calculating primes up to', n,'in 3 seconds')
 time.sleep(3)
 
 primi = []
 num = range(1,n)
 
 io = time.time()
-op = open('primer.txt','w')
+op = open('output.txt','w')
 
 for i in num:
 	flag = 1
@@ -43,9 +51,8 @@ for i in num:
 		op.write(str(i) + '\n')
 		
 to = (time.time()) - io
-print('Tempo necessario per calcolare',len(primi),'numeri primi minori di',n,':',to,'secondi')
-op.write('Tempo totale: ' + str(to) + ' secondi')
+print('Time elapsed calculating',len(primi),'prime numbers <',n,':',to,'seconds')
 
 op.close()
-print('Premere "Invio" per uscire')
+print('Press "Enter" to exit... ')
 input()
